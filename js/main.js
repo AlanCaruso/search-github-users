70594aa95965a43d0de22c44dd851e8d407e7b4e
Alan Caruso <alan.carusoo@gmail.com>
2023-02-20 23:55:48 -0300

    fetch users search

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const API = "https://api.github.com/users/"
 
+
+
+
 const app = Vue.createApp({
     data() {
         return {
-            title: "Search github users"
+            search: null
+        };
+
+    },
+    methods: {
+        async doSearch() {
+            const response = await fetch(API + this.search)
+            const data = await response.json()
+            console.log(data)
         }
-
-
     }
 })
 
